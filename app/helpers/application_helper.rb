283675d1e92f29ff19ae5429a0c78f0e0b71a766
Andrew Ritter <ritterandrew28@gmail.com>
2015-10-25 00:21:22 -0400

    created static pages with routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'from Acorn'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
